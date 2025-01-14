f0990fd70cb6e8931a30234f78d4a77e9e9cc174
maliduhansana772356258 <130507902+maliduhansana772356258@users.noreply.github.com>
2025-01-14 08:29:52 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,7 +11,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `*🧸𝗠𝗔𝗟𝗨𝗗𝗨 𝗛𝗔𝗡𝗦𝗔𝗡𝗔-𝐌𝐃🧸*
+let dec = `*🧸𝗠𝗔𝗟𝗜𝗗𝗨 𝗛𝗔𝗡𝗦𝗔𝗡𝗔-𝐌𝐃🧸*
 
 > *𝙊𝙒𝙉𝙀𝙍* 
 
