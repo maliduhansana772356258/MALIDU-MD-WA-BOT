926c8caf3fecacdebf29d0b471c5d5887785c121
maliduhansana772356258 <130507902+maliduhansana772356258@users.noreply.github.com>
2025-01-15 00:50:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~cytWnTIS#9n_W1Zkc2WQ-OqsDXEJSW4cReyFzHM_Mli8UV9yyQbI",
+SESSION_ID: process.env.SESSION_ID || "s4QVFSKS#LGF7OL_87iDzfpkLj7D0shkJLWLuIcwelVP4ytoy56E",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2DsJTn1Zq-Ot2HfC0AJyyxO813o6pXewVCw&s",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII...I AM ALIVE NOW...😚",
 };
